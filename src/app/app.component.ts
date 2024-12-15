7cb410724c7d5a11119e4e2615204d2a58ce8b8a
Raphaelle <raphaelle.thereze@gmail.com>
2024-12-15 23:21:24 +0100

    add sampo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   persoManon = [
     { name: "genya", statut: "inactif", illu: "https://images2.imgbox.com/52/ae/wccPoc7l_o.png", branche: "lame du duc" },
     { name: "hiyori", statut: "actif", illu: "https://images2.imgbox.com/76/4c/XBJKuhRT_o.png", branche: "maisonnée khensur" },
-    { name: "hoshina", statut: "inactif", illu: "https://i.ibb.co/Z62QDDn/Sans-titre-1.png", branche: "lame du duc "},
+    { name: "hoshina", statut: "actif", illu: "https://i.ibb.co/Z62QDDn/Sans-titre-1.png", branche: "lame du duc "},
     { name: "naoya", statut: "inactif", illu: "https://images2.imgbox.com/ec/88/4SMKlw7s_o.png", branche: "lame du duc"},
     { name: "nori", statut: "inactif", illu: "https://images2.imgbox.com/16/4d/zauLPDoX_o.png", branche: "pirate d'hébi"},
     { name: "rengoku", statut: "inactif", illu: "https://images2.imgbox.com/30/d9/MPJoeuBn_o.png", branche: "lame du duc"},
@@ -31,6 +31,7 @@ export class AppComponent {
     { name: "jotaro", statut: "actif", illu: "https://images2.imgbox.com/02/a8/7sr7i4xk_o.png", branche: "pirate d'hébi"},
     { name: "kise", statut: "inactif", illu: "https://images2.imgbox.com/93/00/CcQobBTv_o.png", branche: "à voir"},
     { name: "osamu", statut: "inactif", illu: "https://images2.imgbox.com/41/60/YMHyJfzz_o.png", branche: "à voir"},
+    { name: "sampo", statut: "actif", illu: "https://images2.imgbox.com/90/1d/taVHtsak_o.png", branche: "lame du duc"},
     { name: "sangwoo", statut: "actif", illu: "https://images2.imgbox.com/51/d0/F0s00Gxz_o.png", branche: "maisonnée khensur"},
     { name: "wakatoshi", statut: "actif", illu: "https://images2.imgbox.com/c7/b4/h81CKiLz_o.png", branche: "lame du duc"},
   ]
